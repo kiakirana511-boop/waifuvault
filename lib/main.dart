01b2db04814f27b311490135dccf3961b37c40c5
kiakirana511-boop <kiakirana511@gmail.com>
2026-06-11 08:49:55 +0700

    Implement V9.3 reference final polish UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-PLACEHOLDER
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as p;
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:video_player/video_player.dart';
+
+void main() => runApp(const WaifuVaultApp());
+
+const Color kBg = Color(0xFF050B1C
